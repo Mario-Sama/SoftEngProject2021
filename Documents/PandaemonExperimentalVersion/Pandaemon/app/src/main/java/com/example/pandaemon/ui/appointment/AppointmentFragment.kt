1b95886dddd8de0a322c06992ce2f0ded996ef8c
Mario-Sama <69966920+Mario-Sama@users.noreply.github.com>
2021-06-08 01:58:25 +0300

    Appointment Backtracking Added

--- Documents/PandaemonExperimentalVersion/Pandaemon/app/src/main/java/com/example/pandaemon/ui/appointment/AppointmentFragment.kt
+++ Documents/PandaemonExperimentalVersion/Pandaemon/app/src/main/java/com/example/pandaemon/ui/appointment/AppointmentFragment.kt
@@ -1,13 +1,16 @@
 package com.example.pandaemon.ui.appointment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.pandaemon.Drawer
 import com.example.pandaemon.R
 import kotlinx.android.synthetic.main.fragment_appointment.*
 
@@ -26,8 +29,15 @@ class AppointmentFragment : Fragment() {
             textView.text = it
         })
         return root
+/*
+        val gobackbutton = findViewById<Button>(R.id.no)
+        gobackbutton.setOnClickListener {
+            val intent = Intent(this, Drawer::class.java)
+            startActivity(intent)
+        }*/
     }
+
     //override fun onBackPressed() {
-        //if(wb_webView.canGoBack()) wb_webView.goBack() else super.onBackPressed()
+    //if(wb_webView.canGoBack()) wb_webView.goBack() else super.onBackPressed()
     //}
 }
