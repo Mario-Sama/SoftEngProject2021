e73f0028d1138b87454db14bd4d489ec0adfdc94
Mystiqye <dionisiskiato@yahoo.com>
2021-06-08 22:38:41 +0300

    Update AppointmentFragment.kt

--- Documents/PandaemonExperimentalVersion/Pandaemon/app/src/main/java/com/example/pandaemon/ui/appointment/AppointmentFragment.kt
+++ Documents/PandaemonExperimentalVersion/Pandaemon/app/src/main/java/com/example/pandaemon/ui/appointment/AppointmentFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.pandaemon.R
 
 class AppointmentFragment : Fragment()  {
@@ -24,6 +26,12 @@ class AppointmentFragment : Fragment()  {
         appointmentViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val yesbutton= root.findViewById<Button>(R.id.yes)
+        yesbutton.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_appointment_to_appointmentSuggestionsFragment)
+        }
+
         return root
 
 
