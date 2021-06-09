562927df63aa499a807618dfd7a5148986b1b0d0
Oathkeeper594 <oathkeeper595@gmail.com>
2021-06-09 16:24:32 +0300

    Multiple Database Tests

--- Documents/Dionisis/Pandaemon/app/src/main/java/com/example/pandaemon/MainActivity.kt
+++ Documents/Dionisis/Pandaemon/app/src/main/java/com/example/pandaemon/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.pandaemon
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-
-
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val getresponse = findViewById<TextView>(R.id.getresponse)
 
         val uploadbutton = findViewById<Button>(R.id.uploadbutton)
         uploadbutton.setOnClickListener {
@@ -19,12 +27,31 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-            val staysafebutton = findViewById<Button>(R.id.staysafebutton)
-            staysafebutton.setOnClickListener {
-                val intent = Intent(this, Drawer::class.java)
-                startActivity(intent)
-            }
-
+        val staysafebutton = findViewById<Button>(R.id.staysafebutton)
+        staysafebutton.setOnClickListener {
+            val intent = Intent(this, Drawer::class.java)
+            startActivity(intent)
+        }
+        val dbtestbutton = findViewById<Button>(R.id.dbtestbutton)
+        dbtestbutton.setOnClickListener {
+            sendGetRequest();
+        }
 
     }
+
+
+        @SuppressLint("SetTextI18n")
+        private fun sendGetRequest() {
+            val queue = Volley.newRequestQueue(this)
+            val url = "http://10.0.2.2/Pandaemon/getdata.php"
+            println("peos")
+            val stringRequest = StringRequest(
+                Request.Method.GET, url,
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    getresponse.setText( "Response is:" +response)
+            }, Response.ErrorListener { getresponse.text = "That didn't work!" })
+            queue.add(stringRequest)
+    }
+
 }
--- Documents/Dionisis/Pandaemon/app/src/main/java/com/example/pandaemon/TestMain.kt
+++ Documents/Dionisis/Pandaemon/app/src/main/java/com/example/pandaemon/TestMain.kt
@@ -1,5 +1,11 @@
 package com.example.pandaemon
 
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
 
 fun main() {
     val ri1= RiskIndexStatistics("scrap", _hasHeatpoints = true, noOfReviews = 7)
@@ -16,4 +22,23 @@ fun main() {
     param1.hasReviews=false
     ri1.calculateRiskIndex()
 
+   // val textView = (R.id.text)
+// ...
+
+// Instantiate the RequestQueue.
+   // val queue = Volley.newRequestQueue(this)
+   // val url = "https://www.google.com"
+
+// Request a string response from the provided URL.
+    /*val stringRequest = StringRequest(
+        Request.Method.GET, url,
+        Response.Listener<String> { response ->
+            // Display the first 500 characters of the response string.
+            textView.text = "Response is: ${response.substring(0, 500)}"
+        },
+        Response.ErrorListener { textView.text = "That didn't work!" })
+
+// Add the request to the RequestQueue.
+    queue.add(stringRequest)
+*/
 }
