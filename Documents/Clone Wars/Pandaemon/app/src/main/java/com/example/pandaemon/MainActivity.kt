3b74856703a6284d5640bc455cc427fc5c7ccea8
Oathkeeper594 <oathkeeper595@gmail.com>
2021-06-10 14:14:31 +0300

    Added Logout Button (on drawer menu xml) and logout functionality

--- Documents/Clone Wars/Pandaemon/app/src/main/java/com/example/pandaemon/MainActivity.kt
+++ Documents/Clone Wars/Pandaemon/app/src/main/java/com/example/pandaemon/MainActivity.kt
@@ -3,15 +3,25 @@ package com.example.pandaemon
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
-
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
+    private companion object{
+        private const val TAG="MainActivity"
+    }
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
 
         val uploadbutton = findViewById<Button>(R.id.uploadbutton)
         uploadbutton.setOnClickListener {
@@ -32,5 +42,20 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean{
+        menuInflater.inflate(R.menu.drawer, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId== R.id.action_logout){
+            Log.i(TAG,"Logged Out")
+            auth.signOut()
+            val logOutIntent = Intent(this, LoginActivity::class.java)
+            logOutIntent.flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logOutIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
